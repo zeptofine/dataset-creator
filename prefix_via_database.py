27acb8bfc0603d8127d1298de494a37ca717efc9
zeptofine <xpsychonauticonx@gmail.com>
2023-07-06 00:06:05 -0400

    replace sort_by_imagehash with prefix_via_database

--- prefix_via_database.py
+++ prefix_via_database.py
@@ -1,28 +1,16 @@
 import argparse
 from pathlib import Path
 
-import imagehash
 import polars as pl
 
 from dataset_filters import DatasetBuilder, HashFilter
 from tqdm import tqdm
 
 
-IMHASH_TYPES = {
-    "average": imagehash.average_hash,
-    "crop_resistant": imagehash.crop_resistant_hash,
-    "color": imagehash.colorhash,
-    "dhash": imagehash.dhash,
-    "dhash_vertical": imagehash.dhash_vertical,
-    "phash": imagehash.phash,
-    "phash_simple": imagehash.phash_simple,
-    "whash": imagehash.whash,
-}
-
-
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="folder to scan", required=True)
+    parser.add_argument("input", help="folder to scan")
+    parser.add_argument("category", help="the category from the database to sort by")
     return parser
 
 
@@ -36,15 +24,15 @@ if __name__ == "__main__":
     exts = [".jpg", ".jpeg", ".png", ".webp"]
     filelist = list(filter(lambda i: i.suffix in exts, folder.rglob("*")))
     db = DatasetBuilder("filedb.feather")
+    assert args.category in db.df.columns, f"selected category is not in {db.df.columns}"
     db.add_filters(HashFilter())
     db.populate_df(filelist)
     file_data = db.df.filter(pl.col("path").is_in(list(map(str, filelist))))
-    with tqdm(file_data.sort("hash").iter_rows(named=True), total=len(file_data)) as t:
+    with tqdm(file_data.iter_rows(named=True), total=len(file_data)) as t:
         for data in t:
             pth = Path(data["path"])
-            hash_ = data["hash"]
-            new_path: Path = new_folder / hash_
-            new_path = new_path.with_name(f"{new_path.stem}_{pth.stem}").with_suffix(pth.suffix)
+            hash_ = str(data[args.category])
+            new_path: Path = (new_folder / f"{hash_}_{pth.stem}").with_suffix(pth.suffix)
             if not new_path.exists():
                 new_path.symlink_to(pth)
                 t.set_description_str(hash_)
